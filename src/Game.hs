9c64ad21dad12fb84ecdd86e123ab12006103d52
Paavo <pparkkin@gmail.com>
2017-12-18 12:33:21 +0800

    Serialize to JSON

--- src/Game.hs
+++ src/Game.hs
@@ -1,26 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Game where
 
+import GHC.Generics
+
+import Data.Aeson ( ToJSON
+                  , toJSON
+                  , object
+                  , (.=)
+                  , encode
+                  )
 import Linear.V2 ( V2(V2) )
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy as BL
 
 import Serializable
 
 data World = World {
       objects :: [ Object ]
-}
+} deriving ( Show, Generic )
 
 data Object = Object {
       pos :: Position,
       vel :: Velocity
-} deriving (Show)
+} deriving ( Show, Generic )
 
 type Position = V2 Double
 type Velocity = V2 Double
 
+instance ToJSON a => ToJSON (V2 a) where
+  toJSON (V2 x y) =
+    object ["x" .= x, "y" .= y]
+instance ToJSON Object
+instance ToJSON World
+
 instance Serializable World where
-  serialize w = BS.concat $ map (C8.pack . show) (objects w)
+  serialize = BL.toStrict . encode
 
 type Second = Double
 

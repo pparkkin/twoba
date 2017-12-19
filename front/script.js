4beec2dfcd0ab76b6ba286b6b5b08f0a703361e6
Paavo <pparkkin@gmail.com>
2017-12-19 08:38:47 +0800

    Render grid

--- front/script.js
+++ front/script.js
@@ -19,3 +19,61 @@ var data = {"grid":
    ["Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead"],
    ["Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead"],
    ["Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead","Dead"]]}
+
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
+function setup() {
+  let app = new Application({
+    backgroundColor: 0xffffff
+  });
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  app.renderer.view.style.backgroundColor = "white";
+  app.renderer.autoResize = true;
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(app.view);
+
+
+  let grid = data["grid"],
+      height = grid.length,
+      cellHeight = innerHeight / height,
+      width = grid[0].length,
+      cellWidth = innerWidth / width;
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == "Live") {
+        let sprite = new Sprite(
+          resources["sprites/sprite.png"].texture
+        );
+        app.stage.addChild(sprite);
+        sprite.width = cellWidth;
+        sprite.height = cellHeight;
+        sprite.x = j * cellWidth;
+        sprite.y = i * cellHeight;
+      }
+    }
+  }
+}
+
+function load() {
+  loader
+    .add("sprites/sprite.png")
+    .load(setup);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // ws = new WebSocket('ws://localhost:3000');
+  // ws.onmessage = function (event) {
+  //   console.log(event.data);
+  // };
+  // ws.onopen = function (event) {
+  //   console.log("Socket open!");
+  //   ws.send("Hello, Server!")
+  // };
+
+  load();
+});

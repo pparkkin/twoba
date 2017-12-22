0acf6aaa536f5be3c3e21d97fc995c58eb5204ec
Paavo <pparkkin@gmail.com>
2017-12-22 10:29:46 +0800

    Removed obsolete test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,38 +5,5 @@ import Game
 main :: IO ()
 main = hspec $ do
   describe "Game" $ do
-    it "should kill cell with one live neighbor" $ do
-      let world = World [[Dead, Dead, Dead],
-                         [Dead, Live, Dead],
-                         [Live, Dead, Dead]]
-      (updateCell world 1 1) `shouldBe` Dead
-    it "should survive cell with two live neighbors" $ do
-      let world = World [[Live, Dead, Live],
-                         [Dead, Live, Dead],
-                         [Dead, Dead, Dead]]
-      (updateCell world 1 1) `shouldBe` Live
-    it "should survive cell with three live neighbors" $ do
-      let world = World [[Live, Dead, Live],
-                         [Dead, Live, Dead],
-                         [Dead, Live, Dead]]
-      (updateCell world 1 1) `shouldBe` Live
-    it "should kill cell with four live neighbors" $ do
-      let world = World [[Live, Dead, Live],
-                         [Dead, Live, Dead],
-                         [Live, Live, Dead]]
-      (updateCell world 1 1) `shouldBe` Dead
-    it "should not resurrect cell with two live neighbors" $ do
-      let world = World [[Dead, Dead, Dead],
-                         [Dead, Dead, Live],
-                         [Dead, Dead, Live]]
-      (updateCell world 1 1) `shouldBe` Dead
-    it "should resurrect cell with three live neighbors" $ do
-      let world = World [[Live, Dead, Dead],
-                         [Dead, Dead, Live],
-                         [Live, Dead, Dead]]
-      (updateCell world 1 1) `shouldBe` Live
-    it "should not resurrect cell with four live neighbors" $ do
-      let world = World [[Dead, Live, Live],
-                         [Dead, Dead, Live],
-                         [Dead, Live, Dead]]
-      (updateCell world 1 1) `shouldBe` Dead
+    it "should have some test cases" $ do
+      False `shouldBe` True
